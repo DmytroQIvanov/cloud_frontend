03f75c95f73cb6968e8c4af88b4725ab421f48ab
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2024-09-30 05:45:57 +0300

    default middleware redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const PUBLIC_FILE = /\.(.*)$/;
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   // alert();
-  console.log("dada");
-  return NextResponse.redirect(new URL("/home", request.url));
+  // console.log("dada");
+  if (
+    request.nextUrl.pathname.startsWith("/_next") ||
+    request.nextUrl.pathname.includes("/api/") ||
+    PUBLIC_FILE.test(request.nextUrl.pathname)
+  ) {
+    return;
+  }
+  // return NextResponse.redirect(new URL("/home", request.url));
 }
 
 // See "Matching Paths" below to learn more
